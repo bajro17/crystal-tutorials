12afe23de8db1078f279c1ad2a3637ec8b7c106d
bajro17 <balisitg@gmail.com>
2018-01-30 22:03:26 +0100

    if and unless explain

---
+++ condition/condition.cr
@@ -0,0 +1,12 @@
+age = 23
+unless age < 18 
+    puts "You dont have 18 years"
+else
+    puts "yes you have 18 years"
+end
+#opposite then unless
+if age < 18 
+    puts "You dont have 18 years"
+else
+    puts "yes you have 18 years"
+end
