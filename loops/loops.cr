08e573ab1147815a37a18ed2fa3a8f2396a8403f
bajro17 <balisitg@gmail.com>
2018-01-30 22:39:09 +0100

    crystal lang loops

---
+++ loops/loops.cr
@@ -0,0 +1,14 @@
+#while
+#until
+#loop
+# age = 1
+# until !(age <= 5)
+#     puts "you dont have 5 years"
+#     age += 1 #age = age + 1 # 1 = 2
+# end
+i = 0
+loop do
+puts i
+i += 1
+break if i == 5
+end
